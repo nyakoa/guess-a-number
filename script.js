2b1c47848f6673ab1b0e5bdd863d45c1abbdad90
nyakoa <126573126+nyakoa@users.noreply.github.com>
2025-03-06 17:00:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function guessNumber() {
       saveData();
     }
 
-    setTimeout(resetGame, 2000);
     document.querySelector(".guess").value = "";
   } else {
     feedback.textContent =
@@ -41,7 +40,6 @@ function guessNumber() {
   if (initialScore === 0) {
     feedback.textContent = "Game Over! Try again.";
     feedback.style.color = "rgb(181, 35, 35)";
-    setTimeout(resetGame, 2000);
   }
 }
 
@@ -53,47 +51,6 @@ function resetGame() {
   feedback.style.color = "#333";
 
   document.querySelector(".guess").value = "";
-}
-
-function saveData() {
-  localStorage.setItem("data", highscore.innerHTML);
-}
-function getData() {
-  highscore.innerHTML = localStorage.getItem("data");
-}
-
-  if (guess === targetNumber) {
-    feedback.textContent = "Correct! You guessed right 🎉.";
-
-    if (highestScore < initialScore) {
-      highestScore = initialScore;
-
-      highscore.textContent = highestScore;
-      saveData();
-    }
-    document.querySelector(".guess").value = "";
-    setTimeout(resetGame, 2000);
-  } else {
-    feedback.textContent =
-      guess > targetNumber ? "Sorry. Too high 📈." : "Sorry. Too low. 📉";
-    initialScore--;
-    score.textContent = initialScore;
-  }
-  if (initialScore === 0) {
-    feedback.textContent = "Game Over! Try again.";
-    feedback.style.color = "rgb(181, 35, 35)";
-    setTimeout(resetGame, 2000);
-  }
-}
-
-function resetGame() {
-  targetNumber = Math.floor(Math.random() * 10) + 1; // Generate new number
-  initialScore = 20; // Reset score
-  score.textContent = initialScore;
-  feedback.textContent = "New game started! Guess again.";
-  feedback.style.color = "#333";
-  document.querySelector(".guess").value = "";
-  getData();
 }
 
 function saveData() {
